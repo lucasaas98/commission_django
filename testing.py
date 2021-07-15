ff24c0f16f18e54bf53d0c2be2753f9e45eea7f4
lucasaas98 <lucasaquilino@live.com.pt>
2021-07-15 23:01:54 +0100

    Added commission_by_city to testing.py

--- testing.py
+++ testing.py
@@ -17,7 +17,7 @@ def total_commission():
     total_commission = 0
     for reservation in reservations:
         if reservation['City'].upper() in rates.keys():
-            total_commission += float(reservation["Net income, EUR"]) * rates[reservation['City']]
+            total_commission += float(reservation["Net income, EUR"]) * rates[reservation['City'].upper()]
         else:
             print(f"No rate for city {reservation['City']}")
     return total_commission
@@ -27,14 +27,22 @@ def total_commission():
 # def commission_per_month():
 
 
-# def commission_by_city(city):
-
+def commission_by_city(city):
+    city = city.upper()
+    total_commission = 0
+    for reservation in reservations:
+        reservation_city = reservation['City'].upper()
+        if (city == reservation_city) and (reservation_city in rates.keys()):
+            total_commission += float(reservation["Net income, EUR"]) * rates[reservation_city]
+    if total_commission == 0:
+        print("There are no current reservations in that city")
+    return total_commission
 
 
 
 
 if __name__ == '__main__':
     print(total_commission())
-    # print(commission_by_city("london"))
+    print(commission_by_city("paris"))
     # print(commission_per_month())
     # print(data)
